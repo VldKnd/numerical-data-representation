44dab325ed86d016d38481881fb9259c0f31aa03
VldKnd <kondratyev.w.i@gmail.com>
2024-11-03 15:41:19 +0100

    feat(ndr): Add tests

--- tests/test_encode_function.py
+++ tests/test_encode_function.py
@@ -18,11 +18,14 @@ class TestEncodeFunction(unittest.TestCase):
             encode_number(input=input, embedding_type=element.value)
 
     def test_encode_numbers_2(self):
-        numerical_input: list[str | int | float ] = [3.14, 10]
+        numerical_input: list[str | int | float] = [3.14, 10]
         for element in EmbeddingClasses:
             encode_numbers(input=numerical_input, embedding_type=element.value)
 
-        string_input: list[str | int | float ] = ["Rated 5 stars", "The product costs 23 dollars"]
+        string_input: list[str | int | float] = [
+            "Rated 5 stars",
+            "The product costs 23 dollars",
+        ]
         for element in EmbeddingClasses:
             encode_numbers(input=string_input, embedding_type=element.value)
 
@@ -34,7 +37,8 @@ class TestEncodeFunction(unittest.TestCase):
             "The product costs 23 dollars",
         ]:
             for element in EmbeddingClasses:
-                encode_numbers(input=input, embedding_type=element.value)
+                encode_number(input=input, embedding_type=element.value)
+
 
 if __name__ == "__main__":
     unittest.main()
